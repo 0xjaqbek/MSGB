2d5d3a5fad0038a2074fd30982d03c91a558df20
0xjaqbek <jaqbek.eth@gmail.com>
2024-10-31 18:53:56 +0100

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@config': path.resolve(__dirname, './src/config'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@types': path.resolve(__dirname, './src/types'),
-    },
-  },
+  plugins: [react(), nodePolyfills()],
+  base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
 });
