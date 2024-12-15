6c0c60c805843444e51b7fa03f4d3db60888aca7
0xjaqbek <jaqbek.eth@gmail.com>
2024-12-15 15:35:11 +0100

    ..

--- src/components/NavigationComponents.tsx
+++ src/components/NavigationComponents.tsx
@@ -54,7 +54,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ currentPage, onNavigate }
         <img 
           src={currentPage === 'main' ? mainActive : mainDefault} 
           alt="Main"
-          style={{ width: '15vh', height: '15vh' }}
+          style={{ width: '13vh', height: '13vh' }}
         />
       </button>
 
@@ -72,7 +72,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ currentPage, onNavigate }
         <img 
           src={currentPage === 'friends' ? friendsActive : friendsDefault} 
           alt="Friends"
-          style={{ width: '15vh', height: '15vh' }}
+          style={{ width: '13vh', height: '13vh' }}
         />
       </button>
 
@@ -90,7 +90,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ currentPage, onNavigate }
         <img 
           src={currentPage === 'account' ? accountActive : accountDefault} 
           alt="Account"
-          style={{ width: '15vh', height: '15vh' }}
+          style={{ width: '13vh', height: '13vh' }}
         />
       </button>
 
@@ -108,7 +108,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ currentPage, onNavigate }
         <img 
           src={currentPage === 'tasks' ? tasksActive : tasksDefault} 
           alt="Tasks"
-          style={{ width: '15vh', height: '15vh' }}
+          style={{ width: '13vh', height: '13vh' }}
         />
       </button>
     </div>
